e6e0134b34efdec597f1502163777774d51e05b0
Bruno <brunoaugustosp@outlook.com.br>
2021-09-06 05:51:32 -0300

    add heroku yml

--- backend/app.py
+++ backend/app.py
@@ -1,22 +1,40 @@
 from flask import *
-from flask_cors import CORS, cross_origin
+
 
 
 
 app = Flask(__name__)
-# ------------------------ CONFIGURAÇÂO CORS API ------------------------------
-cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 
-
-@app.get("/api/teste")
-@cross_origin()
+@app.route("/api/teste", methods=["GET", "POST"])
 def hello():
     return jsonify({"print":"TESTE DE API GET"})
 
 
+@app.route("/")
+def index():
+    return jsonify({"print":"FUNCIONANDO!!"})
 
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
+
+
+
+# from typing import Optional
+# from fastapi import FastAPI
+
+# app = FastAPI()
+
+
+# @app.get("/")
+
+# def read_root():
+#     return {"Hello": "World"}
+
+
+# @app.get("/items/{item_id}")
+
+# def read_item(item_id: int, q: Optional[str] = None):
+#     return {"item_id": item_id, "q": q}
