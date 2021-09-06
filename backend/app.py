857dbfd60e2605c261bd62703e461794c0f69bda
Bruno <brunoaugustosp@outlook.com.br>
2021-09-06 06:26:29 -0300

    teste backend

--- backend/app.py
+++ backend/app.py
@@ -20,7 +20,7 @@ def index():
 
 if __name__ == "__main__":
     port = os.environ.get("PORT",5000)
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=port, debug=True)
 
 
 
