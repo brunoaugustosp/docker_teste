0d897e7adfcc760a2ceab245c3a77792cbc65e98
Bruno <brunoaugustosp@outlook.com.br>
2021-10-18 13:49:59 -0300

    teste com gunicorn

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,5 @@
 from flask import *
-import os
+
 
 
 
@@ -7,7 +7,7 @@ import os
 app = Flask(__name__)
 
 
-@app.route("/api", methods=["GET", "POST"])
+@app.route("/", methods=["GET", "POST"])
 def hello():
     return jsonify({"print":"TESTE DE API GET"})
 
@@ -19,8 +19,8 @@ def index():
 
 
 if __name__ == "__main__":
-    port = os.environ.get("PORT",5000)
-    app.run(host="0.0.0.0", port=port, debug=True)
+
+    app.run(host="0.0.0.0", debug=True)
 
 
 
