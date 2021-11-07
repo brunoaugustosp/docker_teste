451c4a767b361c1e8e496aa46063ed112ac39ead
Bruno <brunoaugustosp@outlook.com.br>
2021-11-07 11:27:41 -0300

    teste

--- backend/app.py
+++ backend/app.py
@@ -4,17 +4,42 @@ from flask import *
 
 
 
+
+
+
 app = Flask(__name__)
 
 
-@app.route("/", methods=["GET", "POST"])
-def hello():
-    return jsonify({"print":"TESTE DE API GET"})
+def draco_today():
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=True)
+        page = browser.new_page()
+        page.goto('https://www.mir4draco.com/')
+        value_draco = page.query_selector('//*[@id="app"]/div/div/main/div/div[3]/div[1]/div/div[1]/div/div/div/a/div[2]/span[1]').inner_text()
+        # print("DRACO "+ value_draco)
+        browser.close()
+
+    json = {
+        "value": value_draco,
+        "color": "purple"
+    }
+
+    return json
 
 
-@app.route("/teste")
+
+
+
+@app.route("/")
 def index():
-    return jsonify({"print":"FUNCIONANDO!!"})
+    
+    vl = draco_today()
+
+    return vl
+
+
+
+
 
 
 
